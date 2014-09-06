4e9cd2bbaa81e04eb801806427b17f7c5a4ebfe4
bmsmith <bms0190@unt.edu>
2014-09-06 15:20:47 -0500

    asdf

--- dm_user.py
+++ dm_user.py
@@ -49,55 +49,7 @@ class User:
 				dm_global.CHANNEL)
 
 		}
-		# Global commands dictionary
-
-		self.GLOBAL_COMMANDS = {
-
-			'welcome_edit': (self.welcome_edit,
-				"%swelcome_edit%s - Changes the welcome banner which displays on connect%s",
-				dm_global.ADMIN),
-
-			'login_edit': (self.login_edit,
-				"%slogin_edit%s - Changes the login banner which displays on login%s",
-				dm_global.ADMIN),
-
-			'newuser_edit': (self.newuser_edit,
-				"%snewuser_edit%s - Changes the banner which displays on creation of a new account%s",
-				dm_global.ADMIN),
-
-			'broadcast': (self.broadcast,
-				"%sbroadcast%s - Broadcasts a message%s",
-				dm_global.ADMIN),
-
-			'ch_perm': (self.change_permissions,
-				"%sch_perm%s - Changes the permissions for a particular user. Format: 'ch_perm <user> <+/-> <permission>'%s",
-				dm_global.ADMIN),
-
-			'get_perm': (self.get_permissions,
-				"%sget_perm%s - Displays a list of possible permissions%s",
-				dm_global.ADMIN),
-
-			'write_helpfile': (self.write_helpfile,
-				"%swrite_helpfile%s - Writes a new helpfile for users to read!%s",
-				dm_global.MODERATOR),
-
-			'silence': (self.silence,
-				"%ssilence%s - Silences a given user. Usage: 'silence <username>'%s",
-				dm_global.MODERATOR),
-
-			'unsilence': (self.unsilence,
-				"%ssilence%s - Unsilences a given user. Usage: 'unsilence <username>'%s",
-				dm_global.MODERATOR),
-
-			'open': (self.open,
-				"%sopen%s - Opens a channel with a title. Usage: 'open <title>'%s",
-				dm_global.ADMIN),
-
-			'shutdown': (self.shutdown,
-				"%sshutdown%s - Shuts down the server%s",
-				dm_global.ADMIN)
-
-		}
+		
 
 	# LOGIN SEQUENCE
 	#
@@ -130,7 +82,7 @@ class User:
 		self.a_last_visit_date = user_data[4]
 		self.a_permissions = user_data[5] # See how pretty and efficient this is? :D
 		self.a_silenced = user_data[6]
-		self.mailbox = dm_comm.Mailbox(self.a_account_name, [self], [])
+		
 
 		# Continue Login Sequence
 		self.message_function = self.login_passwd
@@ -277,10 +229,7 @@ class User:
 		else:
 			command = message
 			args = ""
-		if command[0] == '@':
-			if not self.silenced and command[1:] in dm_comm.MAILBOXES:
-				dm_comm.MAILBOXES[command[1:]].recieve_message(args, self.mailbox)
-		elif command in self.GLOBAL_COMMANDS and self.has_permission(self.GLOBAL_COMMANDS[command][2]):
+		if command in self.GLOBAL_COMMANDS and self.has_permission(self.GLOBAL_COMMANDS[command][2]):
 			self.client.send(self.GLOBAL_COMMANDS[command][0](args))
 			self.client.send(dm_ansi.CLEAR)
 		elif command in dm_comm.CHANNELS:
@@ -343,37 +292,7 @@ class User:
 		else:
 			self.client.send("Passwords do not match!\nNew Password:")
 			self.message_function = self.chpass_new_prompt
-	#
-	# HELPFILE SEQUENCE
-	#
-	# 1. Get the title. Do not accept blank input
-	# 2. Get the text of the file using a MultilineInput
-	# 3. Submit it to the database when the user puts in "end"
-	#
-	def hf_title(self, message):
-		"""
-		Adds a helpfile that users can access by entering the 'help' command followed by the filename.
-		Step 1 (and the setup for 2) of the Helpfile sequence
-		"""
-		if len(message) == 0:
-			return
-		if message in HELPFILES:
-			self.client.send("That file already exists!")
-			return
-		self.helpfile_title = message
-		#Step 2 stuff!
-		helpfile_body = MultilineInput(self.hf_submit, self.activate_standardseq)
-		self.message_function = helpfile_body.input
-		self.client.send("Text:\n(Type 'end' to finish or 'cancel' to cancel)\n")
-	def hf_submit(self, fulltext):
-		"""
-		Finalizes the helpfile and submits it to the database.
-		Step 3 of the helpfile sequence
-		"""
-		dm_global.db_conn.create_helpfile(self.helpfile_title, fulltext)
-		self.message_function = self.standardseq_command;
-		HELPFILES[self.helpfile_title] = fulltext
-		self.helpfile_title = None
+	
 	# USER COMMANDS
 	#
 	# These are for use by everyone. They're even left on for people who are banned,
@@ -385,7 +304,7 @@ class User:
 		"""
 		if args == "":
 			helpstring = "List of Commands: \n\n"
-			for command in self.GLOBAL_COMMANDS:
+			for command in dm_global.COMMANDS:
 				if self.has_permission(self.GLOBAL_COMMANDS[command][2]):
 					helpstring += self.GLOBAL_COMMANDS[command][1] % (dm_ansi.CYAN, dm_ansi.GREEN, dm_ansi.CLEAR + "\n")
 			for command in self.USER_COMMANDS:
@@ -431,137 +350,7 @@ class User:
 				return dm_comm.CHANNELS[args].plug(self)
 		else:
 			return "Channel does not exist!"
-	#
-	# GLOBAL COMMANDS - commands that have to do with server-wide actions or admin level stuff
-	#
-	def silence(self, args):
-		"""
-		Command to shut off user access to all channels.
-		"""
-		if len(args) == 0:
-			return;
-		for user in dm_global.USER_LIST:
-			if user.a_account_name == args:
-				user.a_silenced = True
-				dm_global.db_conn.update_user_silence(user)
-				return "Muted"
-			return "That user doesn't exist!"
-	def unsilence(self, args):
-		"""
-		Command to turn on user access to channels.
-		"""
-		if len(args) == 0:
-			return;
-		for user in dm_global.USER_LIST:
-			if user.a_account_name == args:
-				user.a_silenced = False
-				dm_global.db_conn.update_user_silence(user)
-				return "Unmuted"
-			return "That user doesn't exist!"
-	def broadcast(self, args):
-		"""
-		Send msg to every client.
-		"""
-		for user in dm_global.USER_LIST:
-			if user.logged_in:
-				user.client.send(args)
-		return ""
-	def write_helpfile(self, args):
-		"""
-		Start create helpfile sequence
-		"""
-		self.message_function = self.hf_title
-		self.client.send("Title: ")
 
-	def welcome_edit(self, args):
-		"""
-		Changes the welcome (on connect) banner
-		"""
-		self.client.send("Enter the new banner text:\n(Type 'end' to finish or 'cancel' to cancel)\n")
-		mli = MultilineInput(self.welcome_edit_write, self.activate_standardseq)
-		self.message_function = mli.input
-
-	def welcome_edit_write(self, text):
-		dm_global.db_conn.write_welcome_banner(text)
-		self.message_function = self.standardseq_command
-		dm_global.WELCOME_MESSAGE = text
-	def login_edit(self, args):
-		"""
-		Changes the login banner
-		"""
-		self.client.send("Enter the new banner text:\n(Type 'end' to finish or 'cancel' to cancel)\n")
-		mli = MultilineInput(self.login_edit_write, self.activate_standardseq)
-		self.message_function = mli.input
-	def login_edit_write(self, text):
-		dm_global.db_conn.write_login_banner(text)
-		self.message_function = self.standardseq_command
-		dm_global.LOGIN_MESSAGE = text
-	def newuser_edit(self, args):
-		"""
-		Changes the new user banner
-		"""
-		self.client.send("Enter the new banner text:\n(Type 'end' to finish or 'cancel' to cancel)\n")
-		mli = MultilineInput(self.newuser_edit_write, self.activate_standardseq)
-		self.message_function = mli.input
-	def newuser_edit_write(self, text):
-		dm_global.db_conn.write_newuser_banner(text)
-		self.message_function = self.standardseq_command
-		dm_global.NEW_USER_MESSAGE = text
-	def open(self, args):
-		"""
-		Open a channel
-		"""
-		if args == "":
-			self.client.send("Please enter in the name of the channel you would like to open. Format 'open <name>'\n")
-		else:
-			if len(args) > 128:
-				self.client.send("Channel name too long. Must be < 128 characters\n")
-				return
-			if args in self.GLOBAL_COMMANDS or args in self.USER_COMMANDS:
-				self.client.send("Channel name cannot conflict with an existing command\n")
-			if args in dm_comm.CHANNELS:
-				self.client.send("Channel already exists with that name\n")
-				return
-			dm_comm.Channel(args)
-			dm_global.db_conn.create_channel(args)
-			self.client.send("Channel '%s%s%s' created\n" % (dm_ansi.CYAN, args, dm_ansi.CLEAR))
-			return
-	# NOTE: These commands are for administrators. Moderators can only silence/unsilence.
-	# They can also shadowban.
-	# Even administrators should use these commands instead when dealing with rogue users,
-	# because they send alerts to user inboxes. (Or they will when implemented)
-	# These should be used to make users mods or admins
-	def change_permissions(self, args):
-		"""
-		Change the permissions of a particular user. Args in the format of <user> <+/-> <permission>
-		"""
-		arg_list = args.lstrip().split(' ')
-		if len(arg_list) < 3:
-			return "Too few arguments. Be sure to use the format 'ch_perm <user> <+/-> <permission>'"
-		elif arg_list[1] not in ("+", "-"):
-			return "Invalid argument. Be sure to use the format 'ch_perm <user> <+/-> <permission>'"
-		elif arg_list[2] not in dm_global.PERMS_DICT:
-			return "Invalid permissions. Use the get_perm command to view all valid permission types."
-		else:
-			if arg_list[1] == "-":
-				return dm_global.db_conn.change_permissions_cmd(arg_list[0], -1 * dm_global.PERMS_DICT[arg_list[2]])
-			else:
-				return dm_global.db_conn.change_permissions_cmd(arg_list[0], dm_global.PERMS_DICT[arg_list[2]])
-
-	def get_permissions(self, args):
-		"""
-		Displays a list of permissions groups
-		"""
-		s = ""
-		for key in dm_global.PERMS_DICT.keys():
-			s += "%s\n"%(key)
-		return s
-	def shutdown(self, args):
-		"""
-		Shutdown
-		"""
-		self.broadcast("SYSTEM IS SHUTTING DOWN NOW!")
-		raise dm_global.ExitSignal(0)
 	#
 	# Other misc methods
 	#
@@ -578,7 +367,7 @@ class User:
 		self.a_last_visit_date = None
 		self.a_permissions = None # See how pretty and efficient this is? :D
 		self.a_silenced = None
-		self.mailbox = None
+		
 		# Login Message
 		self.client.send("Username (if this is your first visit, enter in a username to sign up): ")
 		self.logged_in = False # To prevent function calls when data has not yet been populated as necessary for preconditions
@@ -598,29 +387,3 @@ class User:
 
     	def __ne__(self, other):
         	return not self.__eq__(other)
-#
-# MultilineInput - A unique class to add multiline input over telnet.
-#
-
-class MultilineInput:
-	#
-	# Takes two function names as input. The function to call with the full data, and a function to call when quitting
-	#
-	def __init__(self, callback, cancel):
-		self.callback = callback
-		self.text = ""
-		self.cancel = cancel
-
-	def input(self, message):
-		"""
-		Method to add input
-		"""
-		if len(message) == 0:
-			self.text += "\n"
-		elif message == "end":
-			self.callback(self.text)
-		elif message == "cancel":
-			self.cancel()
-		else:
-			self.text += str(message)
-			self.text += "\n"
